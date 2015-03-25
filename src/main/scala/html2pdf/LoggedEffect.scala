f3d359dc0acc60a81727c8b0445ff407b5cd1870
Frank S. Thomas <frank@timepit.eu>
2015-03-25 08:38:29 +0100

    Delete Converter

--- src/main/scala/html2pdf/LoggedEffect.scala
+++ src/main/scala/html2pdf/LoggedEffect.scala
@@ -3,31 +3,42 @@ package html2pdf
 import java.nio.file.Path
 
 import html2pdf.StreamUtil._
+import scodec.bits.ByteVector
 
 import scalaz.concurrent.Task
 import scalaz.stream.Process._
 import scalaz.stream.Writer
 
 object LoggedEffect {
-  def tempFile(prefix: String, suffix: String): Writer[Task, LogEntry, Path] =
-    await(Effect.createTempFile(prefix, suffix)) { path =>
-      val msg = s"Created temporary file ${path.toString}"
-      Log.infoW(msg) ++ emitO(path).onComplete(ignoreO(deleteFile(path)))
-    }
+  def createPdf(url: String): Writer[Task, LogEntry, ByteVector] =
+    Log.infoW(s"C  onverting $url") ++
+      tempFile("h2p-", ".pdf").flatMapO { pdf =>
+        execWkHtmlToPdf(url, pdf) ++ liftW(readFile(pdf))
+      }
 
   def deleteFile(path: Path): Writer[Task, LogEntry, Unit] =
     Log.infoW(s"Deleting ${path.toString}") ++
       evalO(Effect.deleteFile(path))
 
-  def execCmd(cmd: String, args: String*): Writer[Task, LogEntry, String] = {
-    val cmdLine = (cmd +: args).mkString(" ")
-    Log.infoW(s"Executing $cmdLine") ++
-      await(Effect.execCmd(cmd, args: _*))(logCmdResult)
-  }
-
-  def logCmdResult(res: Effect.CmdResult): Writer[Nothing, LogEntry, String] = {
+  def emitCmdResult(res: Effect.CmdResult): Writer[Nothing, LogEntry, String] = {
     def errLog = Log.warnW(res.err)
     def statusLog = Log.errorW(s"${res.cmd} exited with status ${res.status.toString}")
     emitO(res.out) ++ runIf(res.err.nonEmpty)(errLog) ++ runIf(res.status != 0)(statusLog)
   }
+
+  def execCmd(cmd: String, args: String*): Writer[Task, LogEntry, String] = {
+    val cmdLine = (cmd +: args).mkString(" ")
+    Log.infoW(s"Executing $cmdLine") ++
+      await(Effect.execCmd(cmd, args: _*))(emitCmdResult)
+  }
+
+  def execWkHtmlToPdf(input: String, output: Path): Writer[Task, LogEntry, Nothing] =
+    execCmd("wkhtmltopdf-h2p.sh", input, output.toString)
+      .flatMapO(Log.infoW)
+
+  def tempFile(prefix: String, suffix: String): Writer[Task, LogEntry, Path] =
+    await(Effect.createTempFile(prefix, suffix)) { path =>
+      val msg = s"Created temporary file ${path.toString}"
+      Log.infoW(msg) ++ emitO(path).onComplete(ignoreO(deleteFile(path)))
+    }
 }
