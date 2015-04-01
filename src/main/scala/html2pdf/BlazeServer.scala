882111bc0e907c309aa52e8bd5ff1fe33a2663be
Frank S. Thomas <frank@timepit.eu>
2015-04-01 22:57:28 +0200

    Set the server port in the defaults file. close #3

--- src/main/scala/html2pdf/BlazeServer.scala
+++ src/main/scala/html2pdf/BlazeServer.scala
@@ -3,8 +3,12 @@ package html2pdf
 import org.http4s.server.blaze.BlazeBuilder
 
 object BlazeServer extends App {
+  // use Task
+  val port = Option(System.getProperty("HTTP_PORT"))
+    .map(_.toInt).getOrElse(8080)
+
   BlazeBuilder
-    .bindHttp(8080)
+    .bindHttp(port)
     .mountService(Service.route)
     .run
     .awaitShutdown()
