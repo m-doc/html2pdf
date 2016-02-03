04417a03356544136497288d50151d28574a2347
Frank S. Thomas <frank@timepit.eu>
2016-02-04 00:22:31 +0100

    Fix script

--- scripts/cloud-install.sh
+++ scripts/cloud-install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 update_repo() {
+  # http://askubuntu.com/a/65250
   sudo apt-get -y --force-yes update \
     -o Dir::Etc::sourcelist="sources.list.d/$1.list" \
     -o Dir::Etc::sourceparts="-" \
     -o APT::Get::List-Cleanup="0"
 }
 
-ssh -o StrictHostKeyChecking=no -i cloud-identity $CLOUD_HOST \
+chmod 600 cloud-identity
+ssh -q -o StrictHostKeyChecking=no -i cloud-identity $CLOUD_HOST \
   update_repo m-doc && \
   sudo apt-get -y --force-yes install html2pdf
