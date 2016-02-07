012c5b93810ae589f6b7ffa3cf6f2be393e67f69
Frank S. Thomas <frank@timepit.eu>
2016-02-07 10:46:11 +0100

    Use apt-get-update-only

--- scripts/cloud-install.sh
+++ scripts/cloud-install.sh
@@ -5,8 +5,5 @@ sleep 30s # give Bintray time to update the repository
 chmod 600 cloud-identity
 ssh -q -o StrictHostKeyChecking=no -i cloud-identity $CLOUD_HOST \
   "export DEBIAN_FRONTEND=noninteractive && \
-  sudo apt-get -y --force-yes update \
-    -o Dir::Etc::sourcelist=\"sources.list.d/m-doc.list\" \
-    -o Dir::Etc::sourceparts=\"-\" \
-    -o APT::Get::List-Cleanup=\"0\" && \
+  sudo apt-get-update-only -y --force-yes m-doc && \
   sudo apt-get -y --force-yes install html2pdf"
