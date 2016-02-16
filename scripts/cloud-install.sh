8c13351188090a48e39db95c9226d6049a33c208
Frank S. Thomas <frank@timepit.eu>
2016-02-16 23:06:21 +0100

    Suppress debconf warnings once and for all?

--- scripts/cloud-install.sh
+++ scripts/cloud-install.sh
@@ -3,7 +3,6 @@
 sleep 40s # give Bintray time to update the repository
 
 chmod 600 cloud-identity
-ssh -q -o StrictHostKeyChecking=no -i cloud-identity $CLOUD_HOST \
-  "export DEBIAN_FRONTEND=noninteractive && \
-  sudo apt-get-update-only -y --force-yes m-doc && \
-  sudo apt-get -y --force-yes install html2pdf"
+ssh -q -o StrictHostKeyChecking=no -i cloud-identity $CLOUD_HOST "\
+  sudo DEBIAN_FRONTEND=noninteractive apt-get-update-only -y --force-yes m-doc && \
+  sudo DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes install html2pdf"
