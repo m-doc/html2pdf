d6783f396d635723d261a306f8edba334012079b
Frank S. Thomas <frank@timepit.eu>
2016-02-18 21:44:08 +0100

    Do not publish and deploy anymore

--- project/MdocLibrary.scala
+++ project/MdocLibrary.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object MdocLibrary {
-  val fshell = "org.m-doc" %% "fshell" % "0.0.0-41-ga9e9d60"
-  val renderingEngines = "org.m-doc" %% "rendering-engines" % "0.0.0-22-ge276b01"
-  val commonModel = "org.m-doc" %% "common-model" % "0.0.0-15-gdb29b9d"
+  val fshell = "org.m-doc" %% "fshell" % "0.0.0-42-gd54f355"
+  val renderingEngines = "org.m-doc" %% "rendering-engines" % "0.0.0-26-g089cb35"
+  val commonModel = "org.m-doc" %% "common-model" % "0.0.0-16-g36dbae0"
 }
